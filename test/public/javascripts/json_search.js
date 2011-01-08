51aa9a44b1dedcdd900bddaf6a2e64dda24db877
Makoto Inoue <inouemak@googlemail.com>
2011-01-08 23:30:28 +0000

    Refactoring

--- test/public/javascripts/json_search.js
+++ test/public/javascripts/json_search.js
@@ -4,15 +4,17 @@ var JsonSearch = function (list) {
 JsonSearch.prototype.all = function(query){
   var results = []
   var keyword = query.val;
-
+  var append = function(body, value){
+    return body + '["' + value + '"]';
+  };
   var search = function(collection, result){
     for (var i in collection){
       // console.log(i + ":" + collection[i]);
       if (keyword == collection[i]) {
-        results.push(result + '["' + i + '"]')
+        results.push(append(result, i));
       };
       if (typeof(collection[i]) == "object") {
-        result = result + '["' + i + '"]'
+        result = append(result, i);
         search(collection[i], result);
       };
     };
