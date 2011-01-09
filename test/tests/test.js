fbdf2e10ae09a75f5b086a201329cce1e44e2054
Makoto Inoue <inouemak@googlemail.com>
2011-01-09 00:03:17 +0000

    Added test for Array

--- test/tests/test.js
+++ test/tests/test.js
@@ -24,20 +24,39 @@ test("hash seach all by value returns multiple", function() {
   var test = new JsonSearch(hash);
   deepEqual(test.all({val:10}), ['["c"]["age"]', '["d"]["age"]']);
 })
+
+var array = [
+  "A",
+  "B",
+  ["bob", 10],
+  ["mark",10]
+]
+
+test("array seach all by value returns from first depth", function() {
+  var test = new JsonSearch(array);
+  deepEqual(test.all({val:"A"}), ['["0"]']);
+})
+
+test("array seach all by value returns from nested", function() {
+  var test = new JsonSearch(array);
+  deepEqual(test.all({val:"bob"}), ['["2"]["0"]']);
+})
+
+test("array seach all by value returns multiple", function() {
+  var test = new JsonSearch(array);
+  deepEqual(test.all({val:10}), ['["2"]["1"]', '["3"]["1"]']);
+})
+
+// test("combo"), function () {
+//   return false;
+// }
 // 
-// test("hash seach first by value returns one", function() {
-//   var test = new JsonSearch(hash);
-//   deepEqual(test.first({val:10}), ["c"]["age"]);
-// })
-// 
-// test("array"), function () {
+// test("key"), function () {
 //   return false;
 // }
 
-test("combo"), function () {
-  return false;
-}
-
-test("key"), function () {
-  return false;
-}
+// test("hash seach first by value returns one", function() {
+//   var test = new JsonSearch(hash);
+//   deepEqual(test.first({val:10}),  ['["c"]["age"]']);
+// })
+// 
