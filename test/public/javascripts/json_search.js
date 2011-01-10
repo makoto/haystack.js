2a6840cee1130e90a50b15db60a0d1ec54d786a6
Makoto Inoue <inouemak@googlemail.com>
2011-01-10 23:02:13 +0000

    Refactored

--- test/public/javascripts/json_search.js
+++ test/public/javascripts/json_search.js
@@ -4,7 +4,7 @@ JsonSearch = (function () {
   };
 
   function search(searchFunction, collection, prefix){
-    prefix = prefix || "";
+    var prefix = prefix || "";
     var results = [];
     for(var key in collection){
       var value = collection[key];
@@ -21,28 +21,39 @@ JsonSearch = (function () {
     this.list = list;
   }
 
-  klass.prototype.key = function(key, query){
-    var query = query ? query : {};
-
-    var equal = function(k, v){ return k == key};
-    var match = function(k, v){ return k.match(key)};
-
-    var select = query.match ? match : equal;
-    r = search(select, this.list);
+  var setSerachFunctions = function(list, query, callback){
+    var query = query || {};
+    selections = callback();
+    var select = query.match ? selections.match : selections.equal;
+    console.log(select);
+    console.log(list);
+    
+    r = search(select, list);
     console && console.log("RESULT: " + r);
     return r;
+  }
+
+
+  klass.prototype.key = function(key, query){
+    return setSerachFunctions(
+      this.list, query,
+      function(){
+        var equal = function(k, v){ return k == key};
+        var match = function(k, v){ return k.match(key)};
+        return {equal:equal, match:match}
+      }
+    );
   };
 
   klass.prototype.val = function(key, query){
-    var query = query ? query : {};
-
-    var equal = function(k, v){ return v == key};
-    var match = function(k, v){ return v.match(key)};
-
-    var select = query.match ? match : equal;
-    r = search(select, this.list);
-    console && console.log("RESULT: " + r);
-    return r;
+    return  setSerachFunctions(
+      this.list, query,
+      function(){
+        var equal = function(k, v){ return v == key};
+        var match = function(k, v){ return v.match(key)};
+        return {equal:equal, match:match}
+      }
+    );
   };
   return klass;
 })();
