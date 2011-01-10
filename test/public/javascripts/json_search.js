832d0734287c5412878cbb8793bb4fb5b27f0121
Makoto Inoue <inouemak@googlemail.com>
2011-01-10 23:19:34 +0000

    Refactoring

--- test/public/javascripts/json_search.js
+++ test/public/javascripts/json_search.js
@@ -25,33 +25,29 @@ JsonSearch = (function () {
     var query = query || {};
     selections = callback();
     var select = query.match ? selections.match : selections.equal;
-    console.log(select);
-    console.log(list);
-    
     r = search(select, list);
     console && console.log("RESULT: " + r);
     return r;
   }
 
-
   klass.prototype.key = function(key, query){
     return setSerachFunctions(
-      this.list, query,
-      function(){
-        var equal = function(k, v){ return k == key};
-        var match = function(k, v){ return k.match(key)};
-        return {equal:equal, match:match}
+      this.list, query, function(){
+        return {
+          equal:function(k, v){ return k == key}, 
+          match:function(k, v){ return k.match(key)}
+        }
       }
     );
   };
 
   klass.prototype.val = function(key, query){
     return  setSerachFunctions(
-      this.list, query,
-      function(){
-        var equal = function(k, v){ return v == key};
-        var match = function(k, v){ return v.match(key)};
-        return {equal:equal, match:match}
+      this.list, query, function(){
+        return {
+          equal:function(k, v){ return v == key}, 
+          match:function(k, v){ return v.match(key)}
+        }
       }
     );
   };
